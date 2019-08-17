27cb00d69e12a8e6527f0affa1e57bc9be5157de
hyc <992273994@qq.com>
2019-08-17 16:01:28 +0800

    fix banv

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express();
 const path = require('path');
 app.use(express.static('dist'));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {
-        root: path.join(__dirname, './dist')
-    });
+app.use('/', (req, res) => {
+	res.sendFile('index.html', {
+		root: path.join(__dirname, './dist')
+	});
 });
-app.listen(80);
+
+app.listen(3000);
